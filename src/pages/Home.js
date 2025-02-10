bf45552061e2035e12d0eaf0cb4dafd7d19308a1
Iness Ben <ine.benss@gmail.com>
2025-02-10 03:54:49 +0100

    add CSS for the home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,12 @@
 import React from "react";
+import "../styles/Home.css";
 
 function Home() {
-  return <h1>Bienvenue sur la page d&apos;accueil !</h1>;
+  return (
+    <div className="home-container">
+      <h1>Bienvenue sur la page  d&apos;accueil !</h1>
+    </div>
+  );
 }
 
 export default Home;
