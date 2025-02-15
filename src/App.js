5645664a65a061c40d5add6ce7c80940a3c54260
Théa <94928249+TheaCOLINOT@users.noreply.github.com>
2025-02-15 15:16:14 +0100

    add navbar

--- src/App.js
+++ src/App.js
@@ -9,6 +9,15 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          <nav>
+            <h1>Logo</h1>
+            <ul>
+              <li><a href="#">Accueil</a></li>
+              <li><a href="#">Services</a></li>
+              <li><a href="#">Contact</a></li>
+            </ul>
+          </nav>
+
         </p>
         <a
           className="App-link"
