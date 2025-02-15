8a5c2f9f64b75596ed670d5d56be4b03102e2ef3
Théa <94928249+TheaCOLINOT@users.noreply.github.com>
2025-02-15 22:28:58 +0100

    add footer component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 // import Header from "./components/Header";
-// import Footer from "./components/Footer";
+import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
@@ -17,7 +17,7 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path="/about" element={<Contact />} />
       </Routes>
-      {/* <Footer /> */}
+      {/* <Footer />
     </Router>
   );
 }
